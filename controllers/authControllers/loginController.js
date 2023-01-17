ff8974940e663e41d9a1946d9b3df252ecca9f77
Luv Ratan <luvrtn2023@gmail.com>
2023-01-17 22:30:43 +0530

    basic MVC structure setup completed

---
+++ controllers/authControllers/loginController.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const loginController = (req,res)=>{
+
+    return res.send("This route and controller are working just fine")
+
+}
+
+module.exports = loginController;
--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-/** @format */
-
 require("dotenv").config();
 const express = require("express");
 const connectToMongo = require("./config/db");
 connectToMongo();
 const PORT = process.env.PORT;
 
-const app = express();
+const app = express(); //started the express server
 
-app.get("/", (req, res) => {
-  return res.send("API is running");
-});
 
-app.listen(PORT, console.log(`Server has started on port: ${PORT}`));
+app.get('/', (req,res) => res.send("Home Page"));
+app.use('/api/auth', require('./routes/authenticationRoutes'));
+
+
+app.listen(PORT, console.log(`Server has started on port: ${PORT}`)); //started server is on the given port
