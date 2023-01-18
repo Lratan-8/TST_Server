646d3256a63e43e675e8aeeb29ed6c41448d22ce
Luv Ratan <luvrtn2023@gmail.com>
2023-01-18 22:39:41 +0530

    Error handling completed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const connectToMongo = require("./config/db");
+const { notFound, errorHandler } = require("./middlewares/errorHandlingMiddlewares");
 connectToMongo();
 const PORT = process.env.PORT;
 
@@ -9,6 +10,9 @@ app.use(express.json()); //to accept the json data
 
 app.get('/', (req,res) => res.send("Home Page"));
 app.use('/api/auth', require('./routes/authenticationRoutes'));
+
+app.use(notFound);
+app.use(errorHandler);
 
 
 app.listen(PORT, console.log(`Server has started on port: ${PORT}`)); //started server is on the given port
